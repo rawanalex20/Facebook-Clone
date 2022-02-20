f52905059eb84d1ed7f60ab0e81b501a882238fb
Rawan <rawanalex15@gmail.com>
2022-02-20 13:24:32 +0200

    install devise and edit User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :name, presence: true
     validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
     validates :email, presence: true
